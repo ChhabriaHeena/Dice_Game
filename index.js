6e89dc43d2f255c9b857b0240757fbfdde157577
Heena Chhabria <69355836+ChhabriaHeena@users.noreply.github.com>
2022-07-16 13:58:13 +0530

    Dice Game

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1)
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource)
+
+
+// iamge 2 
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1)
+
+var randomDiceImage = "dice" + randomNumber2 + ".png"
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource)
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+} else if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = "It's a Draw"
+
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 wins"
+
+}
